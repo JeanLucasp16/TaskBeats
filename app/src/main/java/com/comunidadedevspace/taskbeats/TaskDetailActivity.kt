b338e50371701dd53be0c0418cb1dba5058244b3
Jean Lucas <jeanlucasp16@gmail.com>
2023-05-06 19:46:18 +0100

    ola

--- app/src/main/java/com/comunidadedevspace/taskbeats/TaskDetailActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskDetailActivity.kt
@@ -37,6 +37,8 @@ class TaskDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
 
+        setSupportActionBar(findViewById(R.id.toolbar))
+
 
          task = intent.getSerializableExtra(TASK_DETAIL_EXTRA) as Task?
         val edtTitle = findViewById<EditText>(R.id.edt_task_title)
