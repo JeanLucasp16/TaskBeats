c1c8e2df6f5f9b35becabf802cc5757079998276
Jean Lucas <jeanlucasp16@gmail.com>
2023-05-09 21:13:34 +0100

    ola

--- app/src/main/java/com/comunidadedevspace/taskbeats/Task.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/Task.kt
@@ -2,11 +2,16 @@ package com.comunidadedevspace.taskbeats
 
 import android.app.ActivityManager.TaskDescription
 import android.icu.text.CaseMap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 import java.io.Serializable
 
+
+@Entity
 data class Task(
-    val id : Int,
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
     val title: String,
     val description: String):  Serializable
 
